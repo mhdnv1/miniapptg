206383d45a2939e9fffabe21313924e3e40bf21d
mhdnv1 <muhidinovfahridin@gmail.com>
2024-07-19 03:27:38 +0600

    device finish

--- src/app/Layout/Layout.tsx
+++ src/app/Layout/Layout.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { Menu } from '../../components/Menu';
 import { Outlet } from 'react-router-dom';
-import Device from '../assets/components/Device/Device';
-
 export const Layout:React.FC = () => {
+    const [isTelegramMobile, setIsTelegramMobile] = useState<boolean>(false);
+    useEffect(() => {
+        if ((window as any).Telegram && (window as any).Telegram.WebApp) {
+          const tg = (window as any).Telegram.WebApp;
+          if (tg && tg.initDataUnsafe) {
+            setIsTelegramMobile(tg.initDataUnsafe.platform === 'mobile');
+          }
+        }
+      }, []);
     return (
-        <div className='container'>
-        <Device/>
-        <Header/>
-        <Outlet/>
-        <Menu/>
-        </div>    
+        <>
+        {
+            isTelegramMobile ? (
+                 <div className='container'>
+                   <Header/>
+                    <Outlet/>
+                   <Menu/>
+                 </div>    
+            ):(
+                <div className='device'>
+                   <p>Available on mobile</p> 
+                </div> 
+            )
+        }
+        </>
     );
 }
