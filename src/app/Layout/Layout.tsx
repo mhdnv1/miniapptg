9216813a40bfde2d7f6760cc3822b625c9cd543d
mhdnv1 <muhidinovfahridin@gmail.com>
2024-07-19 11:36:15 +0600

    true header

--- src/app/Layout/Layout.tsx
+++ src/app/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { Menu } from '../../components/Menu';
 import { Outlet } from 'react-router-dom';
